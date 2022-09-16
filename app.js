3a61d4a03b93ff7d4d9a51bae67294c740c2cc34
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-16 13:44:58 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// initialize variables
+const input = document.getElementById('input')
+const myForm = document.getElementById('form')
+const todos = document.getElementById('todos')
+
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addNewTodo()
+
+})
+
+
+function addNewTodo() {
+    if (input !== "") {
+        console.log('proceed')
+
+        // add newTodo
+        // const newTodos = document.createElement('li')
+        // newTodos.innerHTML = input.value
+        // todos.appendChild(newTodos)
+        
+
+        // try to use for loops
+        
+        // const inputs = input.value
+        // inputs.forEach(inp => {
+        //     todos.innerHTML += `
+        //     <li>
+        //         ${inputs}
+        //     </li>
+        // `
+        // });
+
+        
+
+        // pending todos
+
+
+        // myForm.reset()
+    } else {
+        console.log('please fill in the form')
+    }
+
+    myForm.reset()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
